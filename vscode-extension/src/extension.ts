219adee6874cef463d708cefe9ed598cf28561f8
Quincy <67159014+qu1ncyk@users.noreply.github.com>
2026-03-24 21:54:00 +0100

    Added a VSCode extension

---
+++ vscode-extension/src/extension.ts
@@ -0,0 +1,40 @@
+import * as vscode from "vscode";
+import {
+  ParameterizedLanguageServer,
+  VSCodeUriResolverServer,
+  LanguageParameter,
+} from "@usethesource/rascal-vscode-dsl-lsp-server";
+import { join } from "path";
+
+export function activate(context: vscode.ExtensionContext) {
+  // jar that contains the `pico-lsp` project
+  const bogslLSPJar = `|jar+file://${context.extensionUri.path}/assets/jars/bogsl-lsp.jar!|`;
+  const language = <LanguageParameter>{
+    pathConfig: `pathConfig(srcs=[${bogslLSPJar}])`,
+    name: "BoGSL",
+    extensions: ["dsl"],
+    mainModule: "LanguageServer",
+    mainFunction: "languageServices",
+  };
+  console.log(language);
+  // rascal vscode needs an instance of this class, if you register multiple languages, they can share this vfs instance
+  const vfs = new VSCodeUriResolverServer(false);
+  // this starts the LSP server and connects it to rascal
+  const lsp = new ParameterizedLanguageServer(
+    context,
+    vfs,
+    calcJarPath(context),
+    true,
+    "bogsl", // vscode language ID
+    "BoGSL", // vscode language Title (visible in the right bottom corner)
+    language,
+  );
+  // adding it to the subscriptions makes sure everything is closed down properly
+  context.subscriptions.push(lsp);
+}
+
+function calcJarPath(context: vscode.ExtensionContext) {
+  return context.asAbsolutePath(join(".", "dist", "rascal-lsp"));
+}
+
+export function deactivate() {}
